30ef7d7b608e1d6ae7f63cd0280a0af326ffef9c
ThePooE <thepooe@gmail.com>
2016-11-28 14:59:54 +0700

    Install watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,36 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
+var watchify = require("watchify");
 var tsify = require("tsify");
+var gutli = require("gulp-util");
 var paths = {
     pages: ['src/*.html']
 };
+
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
 
 gulp.task("copy-html", function () {
     return gulp.src(paths.pages)
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", ["copy-html"], function () {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/main.ts'],
-        cache: {},
-        packageCache: {}
-    })
-    .plugin(tsify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest("dist"));
-});
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest("dist"));
+}
+
+gulp.task("default", ["copy-html"], bundle);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutli.log);
 
 /*
 var ts = require("gulp-typescript");
